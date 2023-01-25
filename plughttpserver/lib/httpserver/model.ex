7a0d7d67822210f7ca368119dae44e95776253eb
kanpinar <kanpinar@hotmail.com>
2023-01-25 11:51:21 +0300

    json serialize

---
+++ plughttpserver/lib/httpserver/model.ex
@@ -0,0 +1,114 @@
+defmodule Httpserver.SubmitBetPriceChange do
+  @moduledoc ""
+
+  require Decimal
+
+  @type t :: %__MODULE__{
+          eventDetailBetTypeMapID: integer(),
+          eventDetailBetTypeMapPrice: Decimal.t(),
+          feractionalOdd: binary(),
+          guid: nil | Ecto.UUID
+        }
+
+  defstruct [
+    :eventDetailBetTypeMapID,
+    :eventDetailBetTypeMapPrice,
+    :feractionalOdd,
+    :guid
+  ]
+
+  @spec decode!(map()) :: t()
+  def decode!(%{
+        "eventDetailBetTypeMapID" => event_detail_bet_type_map_id,
+        "eventDetailBetTypeMapPrice" => event_detail_bet_type_map_price,
+        "feractionalOdd" => feractional_odd,
+        "guid" => guid
+      })
+      when is_integer(event_detail_bet_type_map_id) and
+             Decimal.is_decimal(event_detail_bet_type_map_price) do
+    %__MODULE__{
+      eventDetailBetTypeMapID: event_detail_bet_type_map_id,
+      eventDetailBetTypeMapPrice: event_detail_bet_type_map_price,
+      feractionalOdd: feractional_odd,
+      guid: guid
+    }
+  end
+end
+
+defmodule Httpserver.CommandResponseError do
+  @moduledoc ""
+
+  @type t :: %__MODULE__{
+          errorCode: integer(),
+          errorDetail: binary(),
+          eventDetailBetTypeMapID: nil | integer(),
+          guid: Ecto.UUID
+        }
+
+  defstruct [
+    :errorCode,
+    :errorDetail,
+    :eventDetailBetTypeMapID,
+    :guid
+  ]
+
+  @spec decode!(map()) :: t()
+  def decode!(%{
+        "errorCode" => error_code,
+        "errorDetail" => error_detail,
+        "eventDetailBetTypeMapID" => event_detail_bet_type_map_id,
+        "guid" => guid
+      })
+      when is_integer(event_detail_bet_type_map_id) do
+    %__MODULE__{
+      errorCode: error_code,
+      errorDetail: error_detail,
+      eventDetailBetTypeMapID: event_detail_bet_type_map_id,
+      guid: guid
+    }
+  end
+
+end
+
+defmodule Httpserver.SubmitBetResponse do
+  @moduledoc ""
+  alias Httpserver.CommandResponseError
+  alias Httpserver.SubmitBetPriceChange
+
+  @type t :: %__MODULE__{
+          errors: [CommandResponseError.t()],
+          priceChanges: [SubmitBetPriceChange.t()],
+          bets: []
+        }
+
+  defstruct [
+    # %CommandResponseError{}
+    :errors,
+    # %SubmitBetPriceChange {}
+    :priceChanges,
+    # %SubmitBetBetList {}
+    :bets
+  ]
+end
+
+defmodule Httpserver.SubmitBetCommandResponse do
+  @moduledoc ""
+  defstruct [
+    :response_type,
+    :response_message,
+    # %SubmitBetResponse {}
+    :response_object
+  ]
+end
+
+defmodule Httpserver.SubmitBetBetList do
+  @moduledoc ""
+  defstruct [
+    :bet_guid,
+    :bet_id,
+    :ticket_number,
+    :bet_date_Time,
+    :selections,
+    :guid
+  ]
+end
--- plughttpserver/mix.exs
+++ plughttpserver/mix.exs
@@ -22,9 +22,16 @@ defmodule Httpserver.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.5.2"},
-      {:jason, "~> 1.2"},
-      {:poison, "~> 5.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:plug_cowboy, "~> 2.1"},
+      {:poolboy, "~> 1.5"},
+      {:poison, "~> 5.0"},
+      {:cachex, "~> 3.4"},
+      {:decimal, "~> 2.0"},
+      {:ecto_sql, "~> 3.9.2"},
+      {:postgrex, "~> 0.16.5"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
